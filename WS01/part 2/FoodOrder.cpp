91cbcd527fefd94ebfd2269c61cfdb24b202aa2b
Nathan Kong <mhk.nathan@gmail.com>
2024-05-12 12:22:51 -0400

    w1p2

--- WS01/part 2/FoodOrder.cpp
+++ WS01/part 2/FoodOrder.cpp
@@ -1,4 +1,6 @@
 #include "FoodOrder.h"
+#include <cstring>
+#include <string>
 #include <limits>
 
 #define COMMA ','
@@ -12,16 +14,50 @@ namespace seneca {
         istr.ignore(std::numeric_limits<std::streamsize>::max(), delim);
     }
 
+    void FoodOrder::copy(const FoodOrder& that) {
+        // m_customerName is not dynamically allocated. Length is fixed as well
+        std::strcpy(m_customerName, that.m_customerName);
+
+        delete[] m_description;
+        m_description = new char[std::strlen(that.m_description) + 1];
+        std::strcpy(m_description, that.m_description);
+
+        m_price = that.m_price;
+        m_dailySpecial = that.m_dailySpecial;
+    }
+
     FoodOrder::FoodOrder() {}
+
+    FoodOrder::~FoodOrder() {
+        delete[] m_description;
+        m_description = nullptr;
+    }
+
+    FoodOrder::FoodOrder(const FoodOrder& that) {
+        copy(that);
+    }
+
+    FoodOrder& FoodOrder::operator=(const FoodOrder& that) {
+        // Prevent self-assignment
+        if (this != &that)
+            copy(that);
+            
+        return *this;
+    }
 
     void FoodOrder::read(std::istream& istr) {
         if (istr.good()) {
 
-            istr.get(m_customerName, CUSTOMER_NAME_LEN, COMMA);   // Delim is NOT extracted
+            istr.get(m_customerName, CUSTOMER_NAME_LEN, COMMA);   // Delim is NOT extracted for get()
             ignore(istr, COMMA);
 
-            istr.get(m_description, DESCRIPTION_LEN, COMMA);
-            ignore(istr, COMMA);
+            // Allows a C-style null-terminated string of any length
+            std::string description;
+            std::getline(istr, description, COMMA);    // Delim IS extracted in getline()
+
+            delete[] m_description;
+            m_description = new char[description.length() + 1];
+            std::strcpy(m_description, description.c_str());
 
             istr >> m_price;
             ignore(istr, COMMA);
--- WS01/part 2/FoodOrder.h
+++ WS01/part 2/FoodOrder.h
@@ -4,7 +4,6 @@
 #include <iostream>
 
 #define CUSTOMER_NAME_LEN 10
-#define DESCRIPTION_LEN 25
 
 extern double g_taxrate;
 extern double g_dailydiscount;
@@ -12,13 +11,21 @@ extern double g_dailydiscount;
 namespace seneca {
     class FoodOrder {
         char m_customerName[CUSTOMER_NAME_LEN] {};
-        char m_description[DESCRIPTION_LEN] {};
+        char* m_description {};
 
         double m_price {};
         bool m_dailySpecial {};
 
+        void copy(const FoodOrder& that);
+
     public:
         FoodOrder();
+
+        // Rule of 3
+        ~FoodOrder();
+        FoodOrder(const FoodOrder& that);
+        FoodOrder& operator=(const FoodOrder& that);
+
         void read(std::istream& istr);
         void display() const;
     };

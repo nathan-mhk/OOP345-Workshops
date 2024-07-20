cd9890f530419bdc42e151373538db786179fc27
Nathan Kong <mhk.nathan@gmail.com>
2024-07-19 21:05:05 -0400

    w9p1

--- WS09/part 1/ProcessData.cpp
+++ WS09/part 1/ProcessData.cpp
@@ -1,5 +1,13 @@
 // Workshop 9 - Multi-Threading, Thread Class
 
+/**
+ * Name:  Nathan Kong
+ * Email: nkong@myseneca.ca
+ * ID: 150950236
+ * Date: 2024-07-19
+ * I declare that this submission is the result of my own work and I only copied the code that my professor provided to complete my workshops and assignments. This submitted piece of work has not been shared with any other student or 3rd party content provider  
+*/
+
 #include <iostream>
 #include <fstream>
 #include <functional>
@@ -51,10 +59,27 @@ namespace seneca
 		//         into variables "total_items" and "data". Don't forget to allocate
 		//         memory for "data".
 		//       The file is binary and has the format described in the specs.
+		std::ifstream file(filename, std::ios::in | std::ios::binary);
 
+		if (file.good()) {
+			// `char` is 1B
+			char cPtr[sizeof(int)]{};
 
+			// First 4B is the total number of data items
+			file.read(cPtr, sizeof(int));
 
+			// Reinterpret the char* as an int* and dereference it
+			total_items = *(reinterpret_cast<int*>(cPtr));
 
+			data = new int[total_items];
+
+			for (int i = 0; file.good() && i < total_items; ++i) {
+				file.read(cPtr, sizeof(int));
+				data[i] = *(reinterpret_cast<int*>(cPtr));
+			}
+		}
+
+		file.close();
 
 		std::cout << "Item's count in file '"<< filename << "': " << total_items << std::endl;
 		std::cout << "  [" << data[0] << ", " << data[1] << ", " << data[2] << ", ... , "
@@ -69,9 +94,26 @@ namespace seneca
 
 
 	// TODO Implement operator(). See workshop instructions for details.
+	int ProcessData::operator()(const std::string& fileName, double& avg, double& var) {
+		computeAvgFactor(data, total_items, total_items, avg);
+		computeVarFactor(data, total_items, total_items, avg, var);
 
+		std::fstream file(fileName, std::ios::out | std::ios::binary | std::ios::trunc);
 
+		if (file.good()) {
+			char cPtr[sizeof(int)]{};
 
+			// The other way around, reinterpret cPtr as an int* and dereference it
+			*(reinterpret_cast<int*>(cPtr)) = total_items;
+			file.write(cPtr, sizeof(int));
 
+			for (int i = 0; file.good() && i < total_items; ++i) {
+				*(reinterpret_cast<int*>(cPtr)) = data[i];
+				file.write(cPtr, sizeof(int));
+			}
+		}
 
+		file.close();
+		return 0;
+	}
 }
